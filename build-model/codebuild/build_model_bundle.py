fd8d059a49130e05a1f65e17fd88f5365ba145a2
Johannes Langer <jlanger@amazon.de>
2020-08-17 14:40:29 +0200

    Added automation to create the tensorflow lite model and save to S3

---
+++ build-model/codebuild/build_model_bundle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import tensorflow as tf
+import numpy as np
+import urllib, json 
+
+IMG_SIZE = 224
+IMAGE_SHAPE = (IMG_SIZE,IMG_SIZE,3)
+
+def get_labels():
+    urllib.request.urlretrieve("https://storage.googleapis.com/download.tensorflow.org/data/ImageNetLabels.txt", "labels.txt")
+
+def get_model():
+    classifier = tf.keras.applications.MobileNetV2(input_shape=IMAGE_SHAPE,
+                                               include_top=True,
+                                               weights='imagenet')
+    converter = tf.lite.TFLiteConverter.from_keras_model(classifier)
+    tflite_model = converter.convert()
+    with tf.io.gfile.GFile('model.tflite', 'wb') as f:
+        f.write(tflite_model)
+if __name__ == "__main__":
+    # execute only if run as a script
+    get_model()
+    get_labels()
